57e6cfd9983ddac501d48da996ce6cbfd0f1de68
coriolinus <coriolinus@gmail.com>
2016-08-03 11:01:09 +0200

    Refactor into a generate_tiled function

--- src/bin/tiled.rs
+++ src/bin/tiled.rs
@@ -13,6 +13,38 @@ fn reify_fcz(c: Complex64) -> Box<Fn(Complex64) -> Complex64 + Sync> {
     Box::new(move |z| (z * z) + c)
 }
 
+fn generate_tiled(low: f64,
+                  steps: u32,
+                  interval: f64,
+                  tile_edge: u32,
+                  threshold: f64)
+                  -> ImageBuffer<image::Luma<u8>, Vec<u8>> {
+    let interpolate = interpolate_stretch(tile_edge, tile_edge, -1.0, 1.0, -1.0, 1.0);
+
+    let mut output = ImageBuffer::new(tile_edge * steps, tile_edge * steps);
+
+    for (y, imag) in (0..steps).map(|s| (s * tile_edge, low + (s as f64 * interval))) {
+        for (x, real) in (0..steps).map(|s| (s * tile_edge, low + (s as f64 * interval))) {
+            println!("\tGenerating tile for ({} + {}i)", real, imag);
+            let fcz = reify_fcz(Complex64::new(real, imag));
+            let tile = parallel_image(tile_edge, tile_edge, &*fcz, &*interpolate, threshold);
+            if !output.copy_from(&tile, x, y) {
+                println!("FATAL: Failed to copy tile into output.");
+                println!("\tTile at ({}, {}) sized ({}, {})",
+                         x,
+                         y,
+                         tile_edge,
+                         tile_edge);
+                let (width, height) = output.dimensions();
+                println!("\tOutput container dimensions ({}, {})", width, height);
+                panic!();
+            }
+        }
+    }
+
+    output
+}
+
 /// As this isn't really a user-facing program so much as a dev tool,
 /// we just hard-code a bunch of constants here and recompile if we
 /// want to change them.
@@ -21,52 +53,23 @@ fn main() {
     const HIGH: f64 = 1.5;
     const STEPS: u32 = 7;
     const INTERVAL: f64 = (HIGH - LOW) / (STEPS - 1) as f64; // 0.5 in range [-1.5..1.5] with 7
+    const TILE_EDGE: u32 = 200;
+    const THRESHOLD: f64 = 2.0;
 
-    let path = {
+    let output = generate_tiled(LOW, STEPS, INTERVAL, TILE_EDGE, THRESHOLD);
+
+    let file_name = {
         let mut path = env::current_dir().unwrap();
         path.push("tiles");
-        path
+        path.push(&format!("julia_range_{}..{}.png", LOW, HIGH));
+        path.to_string_lossy().into_owned()
     };
 
-    const TILE_EDGE: u32 = 200;
-
-    let interpolate = interpolate_stretch(TILE_EDGE, TILE_EDGE, -1.0, 1.0, -1.0, 1.0);
-
-    for threshold in (1_u8..6).map(|t| t as f64 / 2.0) {
-        println!("For threshold {}:", threshold);
-        let mut output: ImageBuffer<image::Luma<u8>, Vec<u8>> = ImageBuffer::new(TILE_EDGE * STEPS,
-                                                                                 TILE_EDGE * STEPS);
-
-        for (y, imag) in (0..STEPS).map(|s| (s * TILE_EDGE, LOW + (s as f64 * INTERVAL))) {
-            for (x, real) in (0..STEPS).map(|s| (s * TILE_EDGE, LOW + (s as f64 * INTERVAL))) {
-                println!("\tGenerating tile for ({} + {}i)", real, imag);
-                let fcz = reify_fcz(Complex64::new(real, imag));
-                let tile = parallel_image(TILE_EDGE, TILE_EDGE, &*fcz, &*interpolate, threshold);
-                if !output.copy_from(&tile, x, y) {
-                    println!("FATAL: Failed to copy tile into output.");
-                    println!("\tTile at ({}, {}) sized ({}, {})",
-                             x,
-                             y,
-                             TILE_EDGE,
-                             TILE_EDGE);
-                    let (width, height) = output.dimensions();
-                    println!("\tOutput container dimensions ({}, {})", width, height);
-                    panic!();
-                }
-            }
-        }
-
-        let file_name = {
-            let mut file_name = path.clone();
-            file_name.push(&format!("julia_threshold_{}_range_{}..{}.png", threshold, LOW, HIGH));
-            file_name.to_string_lossy().into_owned()
-        };
-
-        println!("\tSaving as {:?}", file_name);
-        if let Err(error) = output.save(file_name) {
-            println!("FATAL: Failed to save image.");
-            println!("\t{}", error);
-            panic!();
-        }
+    println!("\tSaving as {:?}", file_name);
+    if let Err(error) = output.save(file_name) {
+        println!("FATAL: Failed to save image.");
+        println!("\t{}", error);
+        panic!();
     }
+
 }
